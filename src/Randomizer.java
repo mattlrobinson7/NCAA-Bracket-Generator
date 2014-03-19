146a917e0d8f1d05a915e9350db0834c74b7b0d9
Matt Robinson <mattlrobinson7@gmail.com>
2014-03-18 20:57:44 -0700

    Adding Smart Trend feature

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -6,18 +6,15 @@ public class Randomizer {
 	private static Team[] bracket;
 	
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		 
+		Scanner in = new Scanner(System.in);	 
 	    System.out.println("Enter 1 for seed based bracket or 2 "
 	    					+ "for smart trend based bracket: ");
 	    int choice = in.nextInt();
 	    System.out.println(choice);
 	    in.close();
 
-		
 		createBracket();
-		
-		
+				
 		while (bracket.length > 1) {
 			Team[] tempBracket = new Team[bracket.length / 2];
 			for (int i = 0; i < bracket.length / 2; i++) {
@@ -25,21 +22,74 @@ public class Randomizer {
 				Team team1 = bracket[2 * i];
 				Team team2 = bracket[2 * i + 1];
 				if (choice == 2 && bracket.length == 64) {
-					winner = smartFindWinner(team1, team2);
-				/*} else if (choice == 2 && bracket.length == 32) {
-					winner = smartFindWinner(team1, team2, 2);*/
+					winner = smartFindWinnerRound1(team1, team2);
+				} else if (choice == 2 && bracket.length == 32) {
+					winner = smartFindWinnerRound2(team1, team2);
+				} else {
+					winner = findWinner(team1, team2);
 				}
-				winner = findWinner(team1, team2);
 				tempBracket[i] = winner;
-				System.out.print(winner);
+				System.out.print(winner);				
 			}
 			bracket = tempBracket;
 			System.out.println();
-		}
-		
+		}		
 	}
 
-	private static Team smartFindWinner(Team team1, Team team2) {	
+	private static Team smartFindWinnerRound2(Team team1, Team team2) {
+		int team2Wins = 0;
+		int team1Wins = 0;
+		
+		Team highSeed;
+		Team lowSeed;
+		if ((team2.seed >= 11 && team1.seed >= 11) || (team2.seed < 11 && team1.seed < 11)) {
+			return findWinner(team1, team2);
+		} else if (team2.seed >= 11) {
+			highSeed = team2;
+			lowSeed = team1;
+		} else {
+			highSeed = team1;
+			lowSeed = team2;
+		}
+		switch (highSeed.seed) {
+        case 16:
+        		team1Wins = 1; //Never actually happened because a #1 has never lost to #16	
+        		team2Wins = 0;
+                break;
+        case 15:	
+        		team1Wins = 6;
+        		team2Wins = 1;
+				break;
+        case 14:
+        		team1Wins = 15;
+        		team2Wins = 2;
+		        break;
+		case 13:	
+				team1Wins = 19;
+				team2Wins = 6;
+				break;
+		case 12:	
+				team1Wins = 21;
+				team2Wins = 20;
+	            break;
+	    case 11:	
+	    		team1Wins = 24;
+	    		team2Wins = 15;
+				break;
+        default: System.err.println("Seeding Error");
+        		 System.exit(1);;
+                 break;
+		}
+        double random = Math.random() * (team1Wins + team2Wins);
+        
+        if (team2Wins > random) {
+        	return highSeed;
+        } else {
+        	return lowSeed;
+        }
+	}
+
+	private static Team smartFindWinnerRound1(Team team1, Team team2) {	
 		int team2Wins = 0;
         switch (team2.seed) {
             case 16:	
@@ -73,9 +123,9 @@ public class Randomizer {
         
         double random = Math.random() * YEARS * 4;
         if (team2Wins > random) {
-        	return team1;
-        } else {
         	return team2;
+        } else {
+        	return team1;
         }
 	}
 
@@ -108,7 +158,6 @@ public class Randomizer {
         } catch (FileNotFoundException e) {
         	System.err.println("Error opening/reading/writing input or output file.");
             System.exit(1);
-        }
-		
+        }	
 	}
 }
